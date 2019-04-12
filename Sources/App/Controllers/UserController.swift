6a96ec1f43276a9d481385f72a91fc2f3f7acd13
Zach 'Leif' Eriksen <zmeriksen@gmail.com>
2019-04-11 19:59:32 -0500

    Cleaned up code for UserController

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -19,62 +19,73 @@ class UserController: RouteCollection {
     }
     
     func register(_ req: Request) throws -> Future<HTTPStatus> {
-        return try req.content.decode(User.self).flatMap { user in
-            
-            return User.query(on: req).filter(\User.email == user.email).first().flatMap { result in
-                if let _ = result {
-                    return Future.map(on: req) {
-                        return HTTPStatus.badRequest
-                    }
-                }
+        
+        return try req.content.decode(User.self)
+            .flatMap { user in
                 
-                user.password = try BCryptDigest().hash(user.password)
-                
-                return user.save(on: req).map { _ in
-                    return HTTPStatus.accepted
+                return User.query(on: req)
+                    .filter(\User.email == user.email)
+                    .first()
+                    .flatMap { result in
+                        if let _ = result {
+                            return Future
+                                .map(on: req) {
+                                    return HTTPStatus.badRequest
+                            }
+                        }
+                        
+                        user.password = try BCryptDigest().hash(user.password)
+                        
+                        return user.save(on: req)
+                            .map { _ in
+                                return HTTPStatus.accepted
+                        }
                 }
-            }
         }
     }
     
     func login(_ req: Request) throws -> Future<HTTPStatus> {
-        return try req.content.decode(User.self).flatMap { user in
-            return User.authenticate(
-                username: user.email,
-                password: user.password,
-                using: BCryptDigest(),
-                on: req
-                ).map { user in
-                    guard let user = user else {
-                        return HTTPStatus.badRequest
-                    }
-                    
-                    try req.authenticateSession(user)
-                    return HTTPStatus.accepted
-            }
+        return try req.content.decode(User.self)
+            .flatMap { user in
+                return User.authenticate(
+                    username: user.email,
+                    password: user.password,
+                    using: BCryptDigest(),
+                    on: req)
+                    .map { user in
+                        guard let user = user else {
+                            return HTTPStatus.badRequest
+                        }
+                        
+                        try req.authenticateSession(user)
+                        return HTTPStatus.accepted
+                }
         }
     }
     
     func profile(_ req: Request) throws -> Future<User> {
         let user = try req.requireAuthenticated(User.self)
-        return Future.map(on: req) { return user }
+        return Future
+            .map(on: req) { return user }
     }
     
     func updateProfile(_ req: Request) throws -> Future<(User)> {
         let user = try req.requireAuthenticated(User.self)
-        return try req.content.decode(User.self).flatMap { updatedUser in
-            if updatedUser.id != user.id {
-                struct BadAccount: Error {
-                    let desc = "BAD"
+        return try req.content.decode(User.self)
+            .flatMap { updatedUser in
+                if updatedUser.id != user.id {
+                    struct BadAccount: Error {
+                        let desc = "BAD"
+                    }
+                    return req.future(error: BadAccount())
                 }
-                return req.future(error: BadAccount())
-            }
-            return updatedUser.save(on: req)
+                return updatedUser.save(on: req)
         }
     }
     
     func logout(_ req: Request) throws -> Future<HTTPStatus> {
         try req.unauthenticateSession(User.self)
-        return Future.map(on: req) { return HTTPStatus.noContent }
+        return Future
+            .map(on: req) { return HTTPStatus.noContent }
     }
 }
